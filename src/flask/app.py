6ec56f0a863366ed3c83dae11684e01187750aa2
muzak23 <logan.nommensen@gmail.com>
2022-10-14 14:43:32 -0400

    Attempt at user page

--- src/flask/app.py
+++ src/flask/app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+from flask import render_template
+
+from markupsafe import escape
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route('/user/')
+@app.route('/user/<name>')
+def user(name=None):
+    return render_template('src/flask/templates/user.html', name=name)
