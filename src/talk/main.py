1120694eb0744f89ac30ba381990bb3b538ecaea
Andrewslayton <slayton@DESKTOP-QS1UE0R.localdomain>
2022-11-27 03:03:46 -0500

    challenge buttons now update user points

--- src/talk/main.py
+++ src/talk/main.py
@@ -3,9 +3,10 @@ from flask import redirect
 from flask import url_for
 from flask import Blueprint
 from flask_login import current_user
+from flask import request
 
 from .models import User
-
+from flask import Flask
 main = Blueprint('main', __name__)
 
 @main.route("/")
@@ -23,8 +24,21 @@ def user_profile(username):
     user = User.query.filter_by(username=username).first()
     return render_template('user_profile.html', user=user)
 
-@main.route('/challenges/')
+@main.route('/challenges/', methods=["POST","GET"])
 def challenges():
+    if request.method == "POST":
+        if request.form['challenge'] == 'Complete Challenge 3':
+            points3= current_user.points() +300
+            User.set_points(current_user,points3)
+            return render_template('leaderboard.html')
+        elif request.form['challenge'] == 'Complete Challenge 2':
+            points2= current_user.points() +200
+            User.set_points(current_user,points2)
+            return render_template('leaderboard.html')
+        elif request.form['challenge'] == 'Complete Challenge 1':
+            points=current_user.points+100
+            User.set_points(current_user,points)
+            print(current_user.points)
     return render_template('challenges.html')
 
 @main.route('/leaderboard/')
